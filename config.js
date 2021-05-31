6d386d9282186583b7e989c2887014108016e56b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-01 06:34:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM']
+global.linkGC = ['https://chat.whatsapp.com/K40vnyCxk9VC09nWBKFC3y']
 global.owner = ['6282114499086', '6282114499086', '6282114499086']// Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
@@ -7,12 +7,12 @@ global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
-  bsbt: 'https://bsbt-api-rest.herokuapp.com/docs'
+  bsbt: 'https://bsbt-api-rest.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://bsbt-api-rest.herokuapp.com/docs': 'benniismael'
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael'
 }
 
 // Sticker WM
