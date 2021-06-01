1da95a0dabb1c7abb305b126389434ea79736086
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-01 16:58:05 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi', { kata: text }, 'APIKEY'))
+  let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) m.reply(json.jawaban)
+  if (json.status) m.reply(json.result)
   else throw json
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
@@ -10,4 +10,3 @@ handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
-
