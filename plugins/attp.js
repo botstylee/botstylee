c743b5de56e26cf137e476ecf9a98b9fdd4bcaa1
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-02 10:11:26 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,10 +1,25 @@
-let handler = async (m, { conn, text }) => {
-  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler  = async (m, { conn, text }) => {
+  await m.reply('Sedang membuat...')
+  if (text) conn.sendFile(m.chat, 'https://bsbt-api-rest.herokuapp.com/api/maker/attp?apikey=benniismael&text=' + encodeURIComponent(text), 'attp.webp', '', m, false, { asSticker: true })
+  else throw 'Uhm...Teksnya?'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['creator']
-
 handler.command = /^attp$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
 
 module.exports = handler
