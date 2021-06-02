d1aeeb1f61f672aaea5542a6f06a95643ae77ba5
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-03 00:30:56 +0700

    Update tebakgambar_answer.js

--- plugins/tebakgambar_answer.js
+++ plugins/tebakgambar_answer.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.tebakgambar[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakgambar[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.result.jawaban.toLowerCase()) {
+    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].exp += conn.tebakgambar[id][2]
       m.reply(`*Benar!*\n+${conn.tebakgambar[id][2]} XP`)
       clearTimeout(conn.tebakgambar[id][3])
       delete conn.tebakgambar[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
 }
