b405ba18adb73a8aed6ace8ab80d168a907c0354
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-02 17:55:27 +0700

    Update caklontong_answer.js

--- plugins/caklontong_answer.js
+++ plugins/caklontong_answer.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.caklontong[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.caklontong[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
+    if (m.text.toLowerCase() == json.result.result.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].exp += conn.caklontong[id][2]
       m.reply(`*Benar!*\n+${conn.caklontong[id][2]} XP`)
       clearTimeout(conn.caklontong[id][3])
       delete conn.caklontong[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.result.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
 }
