6c7813bf7bcaa9412d3ae2a88127cf1ef3d3df2f
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-03 08:48:34 +0700

    Update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -1,34 +1,38 @@
-// Script By RC047
-// Fitur By Xteam
+// Terimakasih kpd RC047 :v
+// Fitur By Xteams
+// Modified by Nurutomo (Update Xteam :v)
 
 const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, text }) => {
-let [tipe, emoji] = text.split `|`
- try {
-  if (!tipe) throw 'Silahkan masukan tipe emoji\n\nMisal !semoji whatsapp'
-  if (!emoji) throw 'Emoji?'
-  let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopng' + tipe, { emo: emoji }, 'APIKEY'), global.packname, global.author)
-  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-    quoted: m
-  })
- } catch (e) {
-   m.reply('Gagal!')
+const fetch = require('node-fetch')
+const defaultType = 'whatsapp'
+let handler = async (m, { usedPrefix, conn, args, text }) => {
+  let [tipe, emoji] = text.includes('|') ? text.split('|') : args
+  if (tipe && !emoji) {
+    emoji = tipe
+    tipe = defaultType
   }
+  if (!emoji) throw `Silahkan masukan emojinya\n\nMisal ${usedPrefix}semoji whatsapp 😎\n\nList Tipe:
+- whatsapp
+- facebook
+- apple
+- google
+- microsoft`
+  emoji = emoji.trim()
+  tipe = tipe.trim().toLowerCase()
+
+  let res = await fetch(global.API('xteam', '/sticker/emojitopng', { emo: emoji.trim() }, 'APIKEY'))
+  if (!res.ok) throw res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  if (!(tipe in json.result)) tipe = defaultType
+  let stiker = await sticker(null, json.result[tipe].img, global.packname, global.author, [emoji])
+  //   m.reply(`
+  // Tipe: ${tipe}
+  // Emoji: ${emoji}
+  // `.trim())
+  m.reply(stiker)
 }
-handler.help = ['semoji <tipe>|<emoji>']
+handler.help = ['semoji [tipe] <emoji>']
 handler.tags = ['sticker']
-handler.command = /^semoji$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
+handler.command = /^s?emo(ji)?$/i
 module.exports = handler
