42e48ee4aa73c3c11f06eef86020d73eff51aee4
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-04 14:19:09 +0700

    Create cerpen.js

---
+++ plugins/cerpen.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+
+let handler = async(m, { conn, text, usedPrefix }) => {
+
+    
+    await m.reply(global.wait)
+    axios.get(`https://docs-jojo.herokuapp.com/api/cerpen`)
+        .then((res) => {
+          let hasil = `*Judul:* ${res.data.result.title}\n\n*Pengarang:* ${res.data.result.pengarang}\n*Kategori:* ${res.data.result.kategori}\n\n   ${res.data.result.cerpen}`
+            conn.reply(m.chat, hasil, m)
+        })
+        .catch()
+}
+handler.help = ['cerpen']
+handler.tags = ['quetes']
+handler.command = /^(cerpen)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
