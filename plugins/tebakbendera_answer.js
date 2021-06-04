c4cba855ad256a994e982e0cabc3fbbaaf6519f6
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-04 13:42:58 +0700

    Update tebakbendera_answer.js

--- plugins/tebakbendera_answer.js
+++ plugins/tebakbendera_answer.js
@@ -1,20 +1,21 @@
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint tebakbendera/i.test(m.quoted.text)) return
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Tebak Bendera 」/i.test(m.quoted.text)) return !0
   conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-  if (!(id in conn.tebakbendera)) throw 'tebakbendera itu telah berakhir!'
+  if (!(id in conn.tebakbendera)) return m.reply('Soal itu telah berakhir')
   if (m.quoted.id == conn.tebakbendera[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
-      global.DATABASE._data.users[m.sender].exp += conn.tebakbendera[id][2]
-      m.reply(`*Benar!*\n+${conn.tebakbendera[id][2]} XP`)
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
+      global.DATABASE._data.users[m.sender].uang += conn.tebakbendera[id][2]
+      m.reply(`*Benar!*\n+Rp${conn.tebakbendera[id][2]}`)
       clearTimeout(conn.tebakbendera[id][3])
       delete conn.tebakbendera[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
+  return !0
 }
 handler.exp = 0
 
