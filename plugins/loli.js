24e04f18f7fa3ae9a03d459a0287aef41899c5c3
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-06 00:21:34 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,10 +1,21 @@
-let handler = async (m, { conn }) => {
-  await m.reply(global.wait)
-  let res = `https://recoders-area.caliph.repl.co/api/loli`
-  conn.sendFile(m.chat, res, 'loli.jpg', `wangy wangy wangy`, m)
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, args }) => {
+    heum = await fetch(`https://recoders-area.caliph.repl.co/api/loli`)
+    json = await heum.buffer()
+   conn.sendFile(m.chat, json, 'nulis', 'Lomlinya kak', m, false)
 }
-handler.help = ['loli'].map(v => v + ' ')
+handler.help = ['loli']
 handler.tags = ['anime']
-handler.command = /^(loli)$/i
+handler.command = /^loli$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 
 module.exports = handler
