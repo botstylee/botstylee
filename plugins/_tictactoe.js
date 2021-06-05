b360db73180b036ca80490ae6378fa94615df073
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-05 12:55:52 +0700

    Update _tictactoe.js

--- plugins/_tictactoe.js
+++ plugins/_tictactoe.js
@@ -1,13 +1,16 @@
 let handler = m => m
 let debugMode = !1
 
+let winScore = 500
+let playScore = 50
+
 handler.before = function (m) {
     let ok
     let isWin = !1
     let isTie = !1
     let isSurrender = !1
     this.game = this.game ? this.game : {}
-    let room = Object.values(this.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
+    let room = Object.values(this.game).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
     if (room) {
         // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
         if (!/^([1-9]|(me)?nyerah|surr?ender)$/i.test(m.text)) return !0
@@ -49,15 +52,18 @@ handler.before = function (m) {
             room.game._currentTurn = m.sender === room.game.playerX
             isWin = true
         }
+        let winner = isSurrender ? room.game.currentTurn : room.game.winner
         let str = `
-Room ID: ${room.id}
 ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
-
-${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} Menang!` : isTie ? 'Game berakhir' : `Menunggu @${room.game.currentTurn.split('@')[0]}`}
+${isWin ? `@${winner.split('@')[0]} Menang! (+${winScore} XP)` : isTie ? `Game berakhir (+${playScore} XP)` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
+❌: @${room.game.playerX.split('@')[0]}
+⭕: @${room.game.playerO.split('@')[0]}
 Ketik *nyerah* untuk nyerah
+Room ID: ${room.id}
 `.trim()
+        let users = global.DATABASE._data.users
         if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
             room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
         if (room.x !== room.o) m.reply(str, room.x, {
@@ -71,6 +77,9 @@ Ketik *nyerah* untuk nyerah
             }
         })
         if (isTie || isWin) {
+            users[room.game.playerX].exp += playScore
+            users[room.game.playerO].exp += playScore
+            if (isWin) users[winner].exp += winScore - playScore
             if (debugMode) m.reply('[DEBUG]\n' + require('util').format(room))
             delete this.game[room.id]
         }
