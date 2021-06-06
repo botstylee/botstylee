0756648fa07d4c8c70cadeeb9d4d4f6b45f163ae
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-06 19:31:30 +0700

    Update kiss.js

--- plugins/kiss.js
+++ plugins/kiss.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn}) => {
   let { 
 result
 } = json
-let stiker = await sticker(null, result, 'Kiss', '@Kokoronationz')
+let stiker = await sticker(null, result, 'Kiss', '@BOT_STYLE')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
