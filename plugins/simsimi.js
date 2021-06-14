431e84d79a4af278238da5c46e64d9ff763442c9
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-14 18:27:50 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
+  let res = await fetch(global.API('bsbt', '/simsimi', { text: text }, 'APIKEY'))
   let json = await res.json()
   if (json.status) m.reply(json.result)
   else throw json
