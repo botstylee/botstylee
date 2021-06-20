d162290eabdd03b008d702bdc4919ece6da4680b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-20 14:16:18 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -39,6 +39,7 @@ let handler  = async (m, { conn, usedPrefix: _p, DevMode }) => {
       'quotes': 'Quotes',
       'nulis': 'Mager Nulis',
       'database': 'Database',
+      'vote': 'Voting',
       'islam': 'Islam',
       'image': 'Image',
       'anime': 'Anime',
