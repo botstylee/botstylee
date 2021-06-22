620fe3694d0d3a199435bf9e7cce7368fb4cb613
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-22 09:09:30 +0700

    Update _antitroli.js

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -3,9 +3,9 @@ let handler = m => m
 handler.all = async function (m) {
     if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
         m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
+        this.reply('6282114499086@s.whatsapp.net', `Pelaku pengirim bug troli @${m.sender.split`@`[0]}`, null, { contextInfo: { mentionedJid: [m.sender] } })
         // await this.clearMessage(m.chat, m.key)
         await this.modifyChat(m.chat, 'delete', {
-        this.reply('6282114499086@s.whatsapp.net', `Pelaku pengirim bug troli @${m.sender.split`@`[0]}`, null, { contextInfo: { mentionedJid: [m.sender] } })
             includeStarred: false
         }).catch(console.log)
     }
