6fddf5c410a327eae738653720bb56fbcf62b9fd
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-23 09:48:19 +0700

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
-    else global.DATABASE._data.chats.sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)')
+    global.DATABASE._data.chats[m.chat].sWelcome = text
+    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
   } else throw 'Teksnya mana?'
 }
 handler.help = ['setwelcome <teks>']
@@ -11,4 +11,3 @@ handler.tags = ['owner', 'group']
 
 handler.command = /^setwelcome$/i
 module.exports = handler
-
