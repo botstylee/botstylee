a72608d9bf3773a5e5beb0cd0483fde51ad028c7
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-23 10:07:22 +0700

    Update artimimpi.js

--- plugins/artimimpi.js
+++ plugins/artimimpi.js
@@ -1,13 +1,29 @@
-let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  if (!text) return m.reply('Mimpi apa? ')
-  let res = await fetch(global.API('bg',  '/artimimpi', { mimpi: text }))
-  let json = await res.json()
-  if (json.status !== true) throw json
-  conn.reply(m.chat, json.result.trim(), m)
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Silahkan masukan mimpimu', m)
+
+  await m.reply('Searching...')
+	axios.get('https://bsbt-api-rest.herokuapp.com/api/artimimpi/?mimpi=${text}&apikey=benniismael').then ((res) => {
+	 	let hasil = `Arti Mimpimu\n\n${res.data.arti.string}`
+
+    conn.reply(m.chat, hasil, m)
+	})
 }
-handler.help = ['artimimpi'].map(v => v + ' [mimpi]')
-handler.tags = ['kerang']
+handler.help = ['artimimpi'].map(v => v + ' <nama>')
+handler.tags = ['primbon']
 handler.command = /^(artimimpi)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
