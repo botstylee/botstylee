ef51657483099634f5af473068d66d2884ec8584
botstylee <85060672+botstylee@users.noreply.github.com>
2021-06-26 23:21:35 +0700

    Update memecreator.js

--- plugins/memecreator.js
+++ plugins/memecreator.js
@@ -1,18 +1,22 @@
 const uploadImage = require('../lib/uploadImage')
+const uploadFile = require('../lib/uploadFile')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   await m.reply(global.wait)
 try {
   let [text1, text2] = text.split('|')
+  if (!text1) text1 = ' '
+  if (!text2) text2 = ' '
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
-  let url = await uploadImage(img)
-  let meme = `https://docs-jojo.herokuapp.com/api/meme-gen?top=${text1}&bottom=${text2}&img=${url}`
-  conn.sendFile(m.chat, meme, 'meme.jpg', '©BOT_STYLE', m)
+  let url = await uploadImage(img).catch(e => uploadFile(img))
+  let meme = global.API('https://api.memegen.link', `/images/custom/${encodeURIComponent(text1)}/${encodeURIComponent(text2)}.png`, {
+    background: url })
+  conn.sendFile(m.chat, meme, 'meme.jpg', '©Kuriyama-bot', m)
 } catch (e) {
   m.reply('Conversion Failed')
   }
