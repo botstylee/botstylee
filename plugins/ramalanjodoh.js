ed4b46fcdd7acb798a6828f42bc7f2cc66e52d36
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-04 23:57:14 +0700

    Update ramalanjodoh.js

--- plugins/ramalanjodoh.js
+++ plugins/ramalanjodoh.js
@@ -1,18 +1,34 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
-	let [nama1, nama2] = text.split `-`
+	let [nama1, nama2] = text.split `|`
 
-    if (!nama1) return conn.reply(m.chat, 'Masukan nama lu', m)
-    if (!nama2) return conn.reply(m.chat, 'Masukan nama doi', m)
+    if (!nama1) return conn.reply(m.chat, '_Masukkan Namamu!_', m)
+    if (!nama2) return conn.reply(m.chat, '_Masukkan Nama Pasanganmu!_', m)
 
   await m.reply(global.wait)
 	axios.get(`http://nzcha-apii.herokuapp.com/ramalan-jodoh?nama1=${nama1}&nama2=${nama2}`).then ((res) => {
 	 	let hasil = `*kecocokan cinta:* ${res.data.result.hasil}`
 
+    let hasil = `
+*Nama Kamu:* ${res.data.result.namaKamu}
+*Nama Pasangan:* ${res.data.result.namaPasangan}
+
+*_Sisi Positif:_*
+_${res.data.result.positif}_
+
+*_Sisi Negatif:_*
+_${res.data.result.negatif}_
+
+*BOT_STYLE*
+`.trim()
+
     conn.reply(m.chat, hasil, m)
 	})
+   } catch (e) {
+  	m.reply('```Error```')
+  }
 }
-handler.help = ['ramalanjodoh <nama1-nama2>']
+handler.help = ['ramalanjodoh <nama1|nama2>']
 handler.tags = ['primbon']
 handler.command = /^(ramalanjodoh)$/i
 handler.owner = false
