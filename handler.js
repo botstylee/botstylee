97a3a430f1c35567b9d4f49c010acada0c7863ab
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-10 08:50:51 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -29,7 +29,7 @@ module.exports = {
             if (!isNumber(user.healt)) user.healt = 0
             if (!isNumber(user.level)) user.level = 0
             if (!isNumber(user.exp)) user.exp = 0
-            if (!isNumber(user.limit)) user.limit = 10
+            if (!isNumber(user.limit)) user.limit = 100
             if (!isNumber(user.lastclaim)) user.lastclaim = 0
             if (!isNumber(user.money)) user.money = 0
             
