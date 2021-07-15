9ad8568344d72140a8e6a0bd65ca74a7c93406d9
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-15 19:58:02 +0700

    Update ht.js

--- plugins/ht.js
+++ plugins/ht.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn, text }) => {
   conn.hartatahta = conn.hartatahta ? conn.hartatahta : {}
-  if (m.chat in conn.hartatahta) throw 'Masih ada yang sedang membuat\nTeks Harta Tahta\ndi chat ini... tunggu sampai selesai'
+  if (m.chat in conn.hartatahta) throw 'Masih ada yang sedang membuat\nTeks Harta Tahta\ndi chat ini... jadi tunggu sampai selesai ya sayang'
+  if (!text) throw `Uhm...Teksnya mana?`
   else conn.hartatahta[m.chat] = true
   m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
   try {
-    let img = await ht(text ? text : ':v')
-    conn.sendFile(m.chat, img, 'Harta Tahta.png', '*Ini sayang udah jadi hasilnya*', m)
+    let img = await ht(text)
+    conn.sendFile(m.chat, img, 'Harta Tahta.png', '*ini hasilnya sayang* 🥰', m)
   } finally {
     delete conn.hartatahta[m.chat]
   }
@@ -13,7 +14,7 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['tahta <teks>']
 handler.tags = ['creator']
 handler.command = /^((harta)?tahta)$/i
-handler.limit = false
+handler.limit = true
 
 module.exports = handler
 
@@ -35,7 +36,7 @@ function ht(text = '') {
     let yF = `((${pickRandom(['', '-'])}${45 * w / 2048}*${pickRandom(['sin', 'cos'])}(X/${w}*4*PI))+${noise('X', 5, w, 0.8)}+${noise('Y', 2, h, 1)})/1.7+128`
     let fsize = 320 / 2048 * w
     let lh = 1.5
-    let format = ',format=rgb24'
+    let format = ''
     let layers = [
       `[v:0]scale=${s}${format}[im]`,
       textArgs('HARTA', 'black', 'white', fsize, font, '(w-text_w)/2', `(h-text_h)/2-(text_h*${lh})`, w, h) + format + '[top]',
@@ -61,15 +62,15 @@ function ht(text = '') {
     console.log(layers)
     console.log('ffmpeg', ...args)
     spawn('ffmpeg', args)
-    .on('error', reject)
-    .on('close', () => {
-      try {
-        resolve(fs.readFileSync(o))
-        fs.unlinkSync(o)
-      } catch (e) {
-        reject(e)
-      }
-    })
+      .on('error', reject)
+      .on('close', () => {
+        try {
+          resolve(fs.readFileSync(o))
+          fs.unlinkSync(o)
+        } catch (e) {
+          reject(e)
+        }
+      })
     //.stderr.on('data', a => console.log(a+''))
   })
 }
@@ -79,7 +80,7 @@ function noise(_var, depth = 4, s = 1024, freq) {
   for (let i = 0; i < depth; i++) forms.push(
     formula(
       _var,
-      freq * rand(40, 80) * (s / 2048)/ s * ((i + 1) / 5),
+      freq * rand(40, 80) * (s / 2048) / s * ((i + 1) / 5),
       rand(-Math.PI, Math.PI),
       (i + 1) / depth * 8,
       0
@@ -92,7 +93,7 @@ function formula(_var, freq, offset, amp, add) {
   return `(${add.toFixed(3)}+${amp.toFixed(4)}*sin(${offset.toFixed(6)}+2*PI*${_var}*${freq.toFixed(6)}))`
 }
 
-function textArgs(text, background, color, size, fontfile, x = '200' , y = '200', w = 1024, h = 1024) {
+function textArgs(text, background, color, size, fontfile, x = '200', y = '200', w = 1024, h = 1024) {
   return `color=${background}:s=${w}x${h},drawtext=text='${text.replace(/[\\]/g, '\\$&')}':fontfile='${fontfile.replace(/[\\]/g, '\\$&')}':x=${x}:y=${y}:fontsize=${size}:fontcolor=${color}`
 }
 
@@ -103,4 +104,3 @@ function pickRandom(list) {
 function rand(min, max, q = 0.001) {
   return Math.floor((Math.random() * (max - min)) / q) * q
 }
-
