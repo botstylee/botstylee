fc67ae3f4e3cfbc09a2c54464c211e08f0ec1a97
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-21 13:14:47 +0700

    Typo

--- plugins/menu.js
+++ plugins/menu.js
@@ -113,21 +113,7 @@ let handler  = async (m, { conn, usedPrefix: _p, DevMode }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
-    conn.reply(m.chat, ``, m, {
-            contextInfo: {
-                mentionedJid: []
-            }, quoted: {
-                key: {
-                    fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? {
-                        remoteJid: "status@broadcast"
-                    } : {})
-                }, message: {
-                    "imageMessage": {
-                        "mimetype": "image/jpeg", "jpegThumbnail": fs.readFileSync('./src/botstyle.png'), "caption": ""
-                    }
-                }
-            }
-        })
+    conn.reply(m.chat, text.trim(), m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
