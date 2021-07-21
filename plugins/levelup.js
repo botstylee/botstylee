a446b20159ebb7dc23b9b48f3640865a4166d1ba
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-21 10:09:44 +0700

    Update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -3,17 +3,21 @@ let levelling = require('../lib/levelling')
 let handler = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (!levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
-    let { min, max } = levelling.xpRange(user.level, user.exp, global.multiplier)
-    return m.reply(`
-Level *${user.level} (${user.exp}/${max})*
-Kurang *${min} XP* lagi!
-`.trim())
+    let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
+    throw `
+Level *${user.level} (${user.exp - min}/${xp})*
+Kurang *${max - user.exp}* lagi!
+`.trim()
   }
-  user.level++
-  m.reply(`
+  let before = user.level * 1
+	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+	if (before !== user.level) {
+            m.reply(`
 Selamat, anda telah naik level!
-*${user.level - 1}* -> *${user.level}*
-  `.trim())
+*${before}* -> *${user.level}*
+gunakan *.profile* untuk mengecek
+	`.trim())
+        }
 }
 
 handler.help = ['levelup']
