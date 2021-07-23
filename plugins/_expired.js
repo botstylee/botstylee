ebc3ec390d231957d4418210fd8c8124a7830dbc
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-23 08:09:11 +0700

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -1,11 +1,12 @@
 let handler = m => m
-handler.before = async (m, { conn }) => {
+handler.before = async (m) => {
+
     if (m.isGroup && global.DATABASE.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.DATABASE.data.chats[m.chat].expired) {
-            let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-            conn.reply(m.chat, `waktunya *${conn.user.name}* untuk meninggalkan grup :(\n*Chat owner jika ingin sewa bot lagi*`, null, { contextInfo: { mentionedJid: users } }).then(() => {
-                conn.sendContact(m.chat, '6282114499086', 'Benniismael', m).then(() => {
-                    conn.groupLeave(m.chat).then(() => {
+            let users = (await this.groupMetadata(m.chat)).participants.map(u => u.jid)
+            this.reply(m.chat, `waktunya *${this.user.name}* untuk meninggalkan grup`, null, { contextInfo: { mentionedJid: users } }).then(() => {
+                this.sendContact(m.chat, global.owner[0], 'BENNIISMAEL', m).then(() => {
+                    this.groupLeave(m.chat).then(() => {
                         global.DATABASE.data.chats[m.chat].expired = 0
                     })
                 })
@@ -13,4 +14,5 @@ handler.before = async (m, { conn }) => {
         }
     }
 }
+
 module.exports = handler
