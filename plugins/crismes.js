32e40d5283354cfdda13dd19133540b6fc27c382
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-23 09:52:10 +0700

    Update and rename xmas.js to crismes.js

--- plugins/crismes.js
+++ plugins/crismes.js
@@ -2,10 +2,10 @@ let limit = 10
 let handler = async(m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, 'Masukkan text yang Anda inginkan', m)
 if (text > 10) return conn.reply(m.chat, '*Text terlalu panjang sayang!*\n_Maksimal 10 huruf!_', m)
-let link = 'https://bsbt-api-rest.herokuapp.com/api/textpro/xmas?text=' + text + '&apikey=benniismael'
+let link = 'https://api.zeks.xyz/api/crismes?text=' + text + '&apikey=apivinz'
 conn.sendFile(m.chat, link, 'BOTSTYLE.png', '_BOTSTYLE_', m)
 }
-handler.help = ['xmas <text>']
+handler.help = ['crismes <text>']
 handler.tags = ['creator']
 handler.command = /^(3d)$/i
 handler.owner = false
