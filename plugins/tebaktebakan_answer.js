6023b6543d1fea82c4837182e7e39850c468aec3
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-23 09:57:07 +0700

    Update tebaktebakan_answer.js

--- plugins/tebaktebakan_answer.js
+++ plugins/tebaktebakan_answer.js
@@ -1,20 +1,23 @@
+const similarity = require('similarity')
+const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
-  let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Tebak-tebakan 」/i.test(m.quoted.text)) return !0
-  conn.tebaktebakan = conn.tebaktebakan ? conn.tebaktebakan : {}
-  if (!(id in conn.tebaktebakan)) return m.reply('Soal itu telah berakhir')
-  if (m.quoted.id == conn.tebaktebakan[id][0].id) {
-    let json = JSON.parse(JSON.stringify(conn.tebaktebakan[id][1]))
-    // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
-      global.DATABASE._data.users[m.sender].uang += conn.tebaktebakan[id][2]
-      m.reply(`*Benar!*\n+Rp${conn.tebaktebakan[id][2]}`)
-      clearTimeout(conn.tebaktebakan[id][3])
-      delete conn.tebaktebakan[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
-  }
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teka/i.test(m.quoted.text)) return !0
+    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
+    if (!(id in conn.tebakkata)) return m.reply('Soal itu telah berakhir')
+    if (m.quoted.id == conn.tebakkata[id][0].id) {
+        let json = JSON.parse(JSON.stringify(conn.tebakkata[id][1]))
+        if (/^.*teka$/i.test(m.text)) return !0
+        if (m.text.toLowerCase() == json.data.jawaban.toLowerCase().trim()) {
+            global.DATABASE.data.users[m.sender].exp += conn.tebakkata[id][2]
+            m.reply(`*Benar!*\n+${conn.tebakkata[id][2]} XP`)
+            clearTimeout(conn.tebakkata[id][3])
+            delete conn.tebakkata[id]
+        } else if (similarity(m.text.toLowerCase(), json.data.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
+    }
+    return !0
 }
 handler.exp = 0
 
