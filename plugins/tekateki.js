1dc7eac9bbd6ef8063389dad14e65a1857449035
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-23 09:59:04 +0700

    Create tekateki.js

---
+++ plugins/tekateki.js
@@ -0,0 +1,35 @@
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tekateki[id][0])
+        throw false
+    }
+    let res = await fetch(global.API('neoxr', '/api/games/tekateki', {}, 'apikey'))
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+    let caption = `
+${json.data.pertanyaan}
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}tete untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.tekateki[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tekateki[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.data.jawaban}*`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tekateki']
+handler.tags = ['game']
+handler.command = /^tekateki/i
+
+module.exports = handler
