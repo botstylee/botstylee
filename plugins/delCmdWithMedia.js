d967b5f463d7c103e239c1defbe1a44dadeea22f
botstylee <85060672+botstylee@users.noreply.github.com>
2021-07-23 10:04:48 +0700

    Fix

--- plugins/delCmdWithMedia.js
+++ plugins/delCmdWithMedia.js
@@ -2,11 +2,12 @@ module.exports = Object.assign(async function handler(m, { text }) {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
     if (!hash) throw `Tidak ada hash`
-    let sticker = global.DATABASE._data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw 'You have no permission to delete this sticker command'
-    m.reply(`Done!`)
+    let sticker = global.DATABASE.data.sticker
+    if (sticker[hash] && sticker[hash].locked) throw 'Kamu tidak memiliki izin untuk menghapus perintah stiker ini'
+    delete sticker[hash]
+    m.reply(`Berhasil!`)
 }, {
-    help: ['cmd'].map(v => 'del' + v + ' <text>'),
+    help: ['cmd'].map(v => 'del' + v + ' <teks>'),
     tags: ['database'],
     command: ['delcmd']
 })
