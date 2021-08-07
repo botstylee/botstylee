908c1753803cc5a5fcfb1c953977aa4337b83470
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 06:37:15 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -9,12 +9,14 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   //   let tiktok = JSON.stringify(res)
   //   let json = JSON.parse(tiktok)
   //   // m.reply(require('util').format(json))
-  //   await conn.sendVideo(m.chat, json.nowm, '*© stikerin*', m, { thumbnail: await (await fetch(json.nowm)).buffer() })
+  //   await conn.sendVideo(m.chat, json.nowm, '*BOTSTYLE*', m, { thumbnail: await (await fetch(json.nowm)).buffer() })
   // })
 
   let res = await fetch(global.API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
+  // if (!json.status) throw json
+  await m.reply(global.wait)
   await conn.sendVideo(m.chat, json.wm, `${json.caption}\n\n\n\n\n BOTSTYLE`, m, { thumbnail: Buffer.alloc(0) })
 
 }
