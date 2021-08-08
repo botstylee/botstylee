0ff7f8177ec31a10ea32cfe1b88728874fdd25e4
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 07:54:37 +0700

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,7 +1,35 @@
-let { WAConnection: _WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, text }) => {
-  let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-  await conn.groupAdd(m.chat, users)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
+  if (!text) throw `_Masukkan nomor!_ \nContoh:\n\n${usedPrefix + command + ' ' + global.owner[0]}`
+  let _participants = participants.map(user => user.jid)
+  let users = (await Promise.all(
+    text.split(',')
+      .map(v => v.replace(/[^0-9]/g, ''))
+      .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+      .map(async v => [
+        v,
+        await conn.isOnWhatsApp(v + '@s.whatsapp.net')
+      ])
+  )).filter(v => v[1]).map(v => v[0] + '@c.us')
+  let response = await conn.groupAdd(m.chat, users)
+  if (response[users] == 408) throw `_Gagal!_\n\nNomor tersebut telah keluar baru² ini\nHanya bisa masuk lewat *${usedPrefix}link* group`
+  let pp = await conn.getProfilePicture(m.chat).catch(_ => false)
+  let jpegThumbnail = pp ? await (await fetch(pp)).buffer() : false
+  for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
+    let [[jid, {
+      invite_code,
+      invite_code_exp
+    }]] = Object.entries(user)
+    let teks = `Mengundang @${jid.split`@`[0]} menggunakan invite...`
+    m.reply(teks, null, {
+      contextInfo: {
+        mentionedJid: conn.parseMention(teks)
+      }
+    })
+    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Invitation to join my WhatsApp group', jpegThumbnail ? {
+      jpegThumbnail
+    } : {})
+  }
 }
 handler.help = ['add', '+'].map(v => v + ' nomor,nomor')
 handler.tags = ['admin']
@@ -19,4 +47,3 @@ handler.fail = null
 handler.limit = true
 
 module.exports = handler
-
