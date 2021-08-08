c8993bfee416474906337b5d34d828c732a6f89d
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 08:42:09 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -1,33 +1,50 @@
-let fetch = require('node-fetch')
+const axios = require("axios")
+const cheerio = require("cheerio")
+const fetch = require('node-fetch')
 
-let timeout = 100000
-let poin = 4999
-let handler  = async (m, { conn, usedPrefix }) => {
-    conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
-    let id = m.chat
-    if (id in conn.tebakgambar) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
-        throw false
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+  let id = m.chat
+  if (id in conn.tebakgambar) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
+    throw false
+  }
+  let random_level = Math.floor(Math.random() * 136)
+  let random_eq = Math.floor(Math.random() * 20)
+  let json = await axios.get(`https://jawabantebakgambar.net/level-${random_level}/`).then((val) => {
+    let url = "https://jawabantebakgambar.net"
+    const $ = cheerio.load(val.data)
+    let href = $("ul > * > a").eq(random_eq)
+    let jwbn = href.find("span").text()
+    let img = href.find("img").attr("data-src")
+    let src = url + img
+    let petunjuk = jwbn.replace(/[AIUEO|aiueo]/g, "_").toLowerCase()
+    return {
+      img: src,
+      jawaban: jwbn.trim().toLowerCase(),
+      petunjuk,
     }
-    let res = await fetch('https://zahirr-web.herokuapp.com/api/kuis/tebakgambar?apikey=zahirgans')
-    let json = await res.json()
-    if (json.status) throw json
-    let caption = `
+  })
+  let caption = `
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hint untuk hint
+Ketik ${usedPrefix}hint untuk bantuan
 Bonus: ${poin} XP
     `.trim()
-    conn.tebakgambar[id] = [
-      await conn.sendFile(m.chat, json.result.images, 'tebakgambar.jpg', caption, m),
-      json, poin,
-      setTimeout(() => {
-        if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.tebakgambar[id][0])
-        delete conn.tebakgambar[id]
-      }, timeout)
-    ]
-  }
-  handler.help = ['tebakgambar']
-  handler.tags = ['game']
-  handler.command = /^tebakgambar/i
-  
-  module.exports = handler
+  conn.tebakgambar[id] = [
+    // await conn.sendFile(m.chat, json.result.images, 'tebakgambar.jpg', caption, m, false, { thumbnail: await (await fetch(json.result.images)).buffer() })
+    await conn.sendButtonImg(m.chat, caption, await (await fetch(json.img)).buffer(), 'BOTSTYLE', 'BANTUAN', '.hint')
+    ,
+    json, poin,
+    setTimeout(() => {
+      if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakgambar[id][0])
+      delete conn.tebakgambar[id]
+    }, timeout)
+  ]
+}
+handler.help = ['tebakgambar']
+handler.tags = ['game']
+handler.command = /^tebakgambar/i
+
+module.exports = handler
