703d02771da16ca515e5fbde4c1d5873a0b741c7
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 07:08:50 +0700

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -1,11 +1,10 @@
 let handler = m => m
-handler.before = async (m) => {
+handler.before = async function (m) {
 
-    if (m.isGroup && global.DATABASE.data.chats[m.chat].expired != 0) {
+    if (m.isGroup && global.db.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.DATABASE.data.chats[m.chat].expired) {
-            let users = (await this.groupMetadata(m.chat)).participants.map(u => u.jid)
-            this.reply(m.chat, `waktunya *${this.user.name}* untuk meninggalkan grup`, null, { contextInfo: { mentionedJid: users } }).then(() => {
-                this.sendContact(m.chat, global.owner[0], 'BENNIISMAEL', m).then(() => {
+            this.reply(m.chat, `waktunya *${this.user.name}* untuk meninggalkan grup`, null).then(() => {
+                this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         global.DATABASE.data.chats[m.chat].expired = 0
                     })
