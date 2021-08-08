4d238fabf4e802005bfebc5728a305222e53b9dc
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 07:14:38 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,12 +6,13 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
+        if (m.text == "") return
         let res = await fetch('https://fdciabdul.tech/api/ayla/?pesan=' + encodeURIComponent(m.text))
         let json = await res.json()
         if (json.jawab == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
-        else await m.reply(`*Simi:* ${json.jawab}`)
+        else await m.reply(`*Simi:* ${json.jawab.replace(/ayla/i, 'stikerin')}`)
         return !0
     }
-    return true
+    return !0
 }
 module.exports = handler
