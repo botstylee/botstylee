3596abe056988ff63d6445d61dd7f7b3111f0e8c
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 08:45:11 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,23 +1,43 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
+let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, text }) => {
   let stiker = false
   try {
-    let [packname, ...author] = text.split('|')
-    author = (author || []).join('|')
+    let [packname, ...author] = text.split`|`
+    author = (author || []).join`|`
+    let q = m.quoted ? m.quoted : m
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw 'Reply sticker!'
-    let img = await m.quoted.download()
-    stiker = await sticker(img, false, packname || global.packname, author || global.author)
+    if (/webp/.test(mime)) {
+      let img = await q.download()
+      let out = await webp2png(img)
+      if (!img) throw `balas stiker dengan perintah ${usedPrefix + command} <packname>|<author>`
+      stiker = await sticker(0, out, packname || '', author || '')
+    } else if (/image/.test(mime)) {
+      let img = await q.download()
+      let link = await uploadImage(img)
+      if (!img) throw `balas gambar dengan perintah ${usedPrefix + command} <packname>|<author>`
+      stiker = await sticker(0, link, packname || '', author || '')
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
+      let img = await q.download()
+      let link = await uploadFile(img)
+      if (!img) throw `balas video dengan perintah ${usedPrefix + command} <packname>|<author>`
+      stiker = await sticker(0, link, packname || '', author || '')
+    }
   } finally {
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Conversion failed'
+    else throw 'Balas stikernya sayang!'
   }
 }
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^wm$/i
+
+handler.limit = true
 
 module.exports = handler
