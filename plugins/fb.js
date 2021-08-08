29651fb9d509606fe01504c726434cd27bd1f76b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-08 08:07:17 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,13 +1,13 @@
 const { facebook } = require('../lib/facebook')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Contoh penggunaan:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (!args[0]) throw `uhm.. url nya mana sayang?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
   facebook(args[0]).then(async res => {
     let fb = JSON.stringify(res)
     let json = JSON.parse(fb)
     m.reply(require('util').format(json))
     if (!json.status) throw json
     await m.reply(global.wait)
-    await conn.sendVideo(m.chat, json.data[1] != undefined ? json.data[1].url : json.data[0].url, 'BOTSTYLE', m)
+    await conn.sendVideo(m.chat, json.data[0].url, 'BOTSTYLE', m)
   }).catch(_ => _)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
