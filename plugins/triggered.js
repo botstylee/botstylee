f4a59004531f4ff081416237378c29461bbd187b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-09 23:04:09 +0700

    Update triggered.js

--- plugins/triggered.js
+++ plugins/triggered.js
@@ -1,12 +1,12 @@
 const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   })
   let stiker = await sticker(null, marah, global.packname, global.author)
- if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  if (stiker) return await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
   throw stiker.toString()
