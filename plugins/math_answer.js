94695b063b46d0f30846f99ccbbb6516d366e0e9
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-09 23:25:00 +0700

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -1,26 +1,28 @@
-global.math = global.math ? global.math : {}
-let handler = async (m, { conn }) => {
+let handler = m => m
+handler.before = async function (m) {
+  if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
   let id = m.chat
-  if (!m.quoted || m.quoted.sender != conn.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) throw false
-  if (!(id in global.math)) throw 'Soal itu telah berakhir'
-  if (m.quoted.id == global.math[id][0].id) {
-  let math = global.math[id][1]
-  if (m.text == math.result) {
-    conn.reply(m.chat, `*Jawaban Benar!*\n+${math.bonus} XP`, m)
-    global.DATABASE._data.users[m.sender].exp += math.bonus
-    clearTimeout(global.math[id][3])
-    delete global.math[id]
-  } else {
-    if (--global.math[id][2] == 0) {
-      conn.reply(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, m)
-      clearTimeout(global.math[id][3])
-      delete global.math[id]
-    } else conn.reply(m.chat, `*Jawaban Salah!*\nMasih ada ${global.math[id][2]} kesempatan`, m)
-  }
- }
+  // if (!m.quoted || m.quoted.sender != this.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) return !0
+  this.math = this.math ? this.math : {}
+  try {
+    if (!(id in this.math) && /^Berapa hasil dari/i.test(m.quoted.text)) return m.reply('Soal itu telah berakhir')
+    // if (m.quoted.id == this.math[id][0].id) {
+    let math = JSON.parse(JSON.stringify(this.math[id][1]))
+    if (m.text == math.result) {
+      global.DATABASE.data.users[m.sender].exp += math.bonus
+      clearTimeout(this.math[id][3])
+      delete this.math[id]
+      await this.sendButton(m.chat, `*Benar!* +${math.bonus} XP`, '', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
+    } else {
+      if (--this.math[id][2] == 0) {
+        clearTimeout(this.math[id][3])
+        delete this.math[id]
+        await this.sendButton(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, '', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
+      } else m.reply(`*Jawaban Salah!*\nMasih ada ${this.math[id][2]} kesempatan`)
+    }
+  } catch (e) { return }
+  // }
+  return !0
 }
-handler.customPrefix = /^-?[0-9]+(\.[0-9]+)?$/
-handler.command = new RegExp
-handler.exp = 0
 
 module.exports = handler
