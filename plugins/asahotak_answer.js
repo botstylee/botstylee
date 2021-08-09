0fe86aaec6a267bd6861ac92d58c1554d4982ae2
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-09 23:33:39 +0700

    Update asahotak_answer.js

--- plugins/asahotak_answer.js
+++ plugins/asahotak_answer.js
@@ -7,14 +7,14 @@ handler.before = async function (m) {
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
     if (/^(me)?nyerah$/i.test(m.text)) {
-        await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.asahotak[id][1].jawaban))}`.trim(), 'kok nyerah t_t', 'ASAH OTAK', '.asahotak').then(() => { delete conn.asahotak[id] })
+        await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.asahotak[id][1].jawaban))}`.trim(), 'kok nyerah t_t', 'ASAH OTAK', '.asahotak').then(() => { delete this.asahotak[id] })
     }
     // if (m.quoted.id == this.asahotak[id][0].id) {
     let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
     if (/^.*ao$/i.test(m.text)) return !0
     if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
         global.DATABASE.data.users[m.sender].exp += this.asahotak[id][2]
-        await this.sendButton(m.chat, `*Benar!*\n+${this.asahotak[id][2]} XP`.trim(), '', 'ASAH OTAK', '.asahotak')
+        await this.sendButton(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`.trim(), '', 'ASAH OTAK', '.asahotak')
         clearTimeout(this.asahotak[id][3])
         delete this.asahotak[id]
     } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
