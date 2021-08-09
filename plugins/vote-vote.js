7bcbfff97aa3cc02958bcbd21efd461ce3743a09
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-09 22:47:58 +0700

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -1,7 +1,10 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) throw `_*tidak ada voting digrup ini!*_\n\n*${usedPrefix}mulaivote* - untuk memulai vote`
+    if (!(id in conn.vote)) {
+        await conn.sendButton(m.chat, `_*tidak ada voting digrup ini!*_`, 'BOTSTYLE', 'MULAI VOTE', `${usedPrefix}mulaivote`)
+        throw false
+    }
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
     if (wasVote) throw 'Kamu sudah vote!'
@@ -10,25 +13,20 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     } else if (/de/i.test(command)) {
         conn.vote[id][2].push(m.sender)
     }
-    m.reply(`Done!\n\n*${usedPrefix}cekvote* - untuk mengecek vote`)
     let [reason, upvote, devote] = conn.vote[id]
     let mentionedJid = [...upvote, ...devote]
-    m.reply(`
-*「 VOTE 」*
+    let caption = `
+    〔 VOTE 〕
 *Alasan:* ${reason}
-
 *UPVOTE*
 _Total: ${upvote.length}_
 ${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
-
 *DEVOTE*
 _Total: ${devote.length}_
 ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
-
-*${usedPrefix}hapusvote* - untuk menghapus vote
-
-_by Benniismael_
-`.trim(), false, { contextInfo: { mentionedJid } })
+_by ariffb_
+    `.trim()
+    await conn.send2Button(m.chat, caption, 'BOSTYLE', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`, { contextInfo: { mentionedJid } })
 }
 handler.help = ['upvote', 'devote']
 handler.tags = ['vote']
