25335aecc3e7b0779ba3403dc5457d32ff68d2ac
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 02:05:27 +0700

    Update susunkata_hint.js

--- plugins/susunkata_hint.js
+++ plugins/susunkata_hint.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.susunkata)) throw false
     let json = conn.susunkata[id][1]
-    let nya = json.result.jawaban
-    let nyanya = nya.replace(/[BCDFGHJKLMNPQRSTVWXYZ]/g, '_')
-    m.reply('```' + nyanya + '```')
+    let ans = json.jawaban.trim()
+    let clue = ans.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.susunkata[id][0])
 }
-handler.command = /^skhint$/i
+handler.command = /^suka$/i
 handler.limit = true
 module.exports = handler
