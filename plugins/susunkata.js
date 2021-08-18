1d4660046b658893b3d9395e8a0f226d751125f7
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 02:01:02 +0700

    Update susunkata.js

--- plugins/susunkata.js
+++ plugins/susunkata.js
@@ -1,38 +1,36 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 2500
-let handler  = async (m, { conn, usedPrefix }) => {
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
     if (id in conn.susunkata) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/susunkata', {}, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
-*「 Susun Kata 」*
-${json.result.level}
-Soal: "${json.result.soal}"
+${json.soal}
+Tipe: ${json.tipe}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik *${usedPrefix}skhint* untuk bantuan
-Bonus: Rp${poin}
-    `.trim()
+Ketik ${usedPrefix}suka untuk bantuan
+Bonus: ${poin} XP
+`.trim()
     conn.susunkata[id] = [
-      await conn.reply(m.chat, caption, m),
-      json, poin,
-      setTimeout(() => {
-        if (conn.susunkata[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.susunkata[id][0])
-        delete conn.susunkata[id]
-      }, timeout)
+        await conn.send2Button(m.chat, caption, 'BOTSTYLE', 'BANTUAN', '.suka', 'NYERAH', 'nyerah'),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'BOTSTYLE', 'SUSUN KATA', '.susunkata')
+            delete conn.susunkata[id]
+        }, timeout)
     ]
-  }
-  handler.help = ['susunkata']
-  handler.tags = ['game']
-  handler.command = /^susunkata/i
-  
-  
-  module.exports = handler
+}
+handler.help = ['susunkata']
+handler.tags = ['game']
+handler.command = /^susunkata/i
+
+module.exports = handler
