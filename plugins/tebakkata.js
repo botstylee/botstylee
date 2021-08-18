dd4f5011f9bf553d7a6db996917bfd6277e8d152
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 01:46:56 +0700

    Update tebakkata.js

--- plugins/tebakkata.js
+++ plugins/tebakkata.js
@@ -1,37 +1,35 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 2500
-let handler  = async (m, { conn, usedPrefix }) => {
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
     if (id in conn.tebakkata) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/tebakkata', {}, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
-*「 Tebak Kata 」*
-${json.result.level}
-Soal: "${json.result.soal}"
+${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik *${usedPrefix}tkhint* untuk bantuan
-Bonus: Rp${poin}
-    `.trim()
+Ketik ${usedPrefix}teka untuk bantuan
+Bonus: ${poin} XP
+`.trim()
     conn.tebakkata[id] = [
-      await conn.reply(m.chat, caption, m),
-      json, poin,
-      setTimeout(() => {
-        if (conn.tebakkata[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.tebakkata[id][0])
-        delete conn.tebakkata[id]
-      }, timeout)
+        await conn.send2Button(m.chat, caption, 'BOTSTYLE', 'BANTUAN', '.teka', 'NYERAH', 'nyerah'),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tebakkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'BOTSTYLE', 'TEBAK KATA', '.tebakkata')
+            delete conn.tebakkata[id]
+        }, timeout)
     ]
-  }
-  handler.help = ['tebakkata']
-  handler.tags = ['game']
-  handler.command = /^tebakkata/i
-  
-  module.exports = handler
+}
+handler.help = ['tebakkata']
+handler.tags = ['game']
+handler.command = /^tebakkata/i
+
+module.exports = handler
