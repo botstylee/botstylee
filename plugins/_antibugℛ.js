96c52a5654ab855c9e70c6fdabc2b31cd223f01e
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-20 00:39:59 +0700

    Update _antibugℛ.js

--- plugins/_antibugℛ.js
+++ plugins/_antibugℛ.js
@@ -7,9 +7,9 @@ handler.before = function (m, { user, isAdmin, isBotAdmin }) {
   if (m.isBaileys && m.fromMe) throw false
   let chat = global.DATABASE.data.chats[m.chat]
   let name = this.getName(m.sender)
-  let bugℛ = linkRegex.exec(m.text)
+  let Bugℛ = linkRegex.exec(m.text)
 
-  if (chat.antiBugℛ && bugℛ) {
+  if (chat.antiBugℛ && Bugℛ) {
  m.reply(`*「 ANTI BUG ℛ 」*\n\nTerdeteksi *${name}* bocah telah mengirim bug ℛ !\n\nMaaf Bocah Bug ℛ Tidak Berguna Bagi Bot!`)
    await this.modifyChat(m.chat, 'clear', {
             includeStarred: false
