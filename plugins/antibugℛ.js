4372071f849975f7b938a4644b38d80e2d8a0e92
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 23:46:32 +0700

    Create antibugℛ.js

---
+++ plugins/antibugℛ.js
@@ -0,0 +1,20 @@
+let handler = m => m
+
+let linkRegex = /ℛ/i
+
+handler.before = function (m, { user, isAdmin, isBotAdmin }) {
+
+  if (m.isBaileys && m.fromMe) throw false
+  let chat = global.DATABASE.data.chats[m.chat]
+  let name = this.getName(m.sender)
+  let Bugℛ = linkRegex.exec(m.text)
+
+  if (chat.antiBugℛ && Bugℛ) {
+ m.reply(`*「 ANTI BUG ℛ 」*\n\nTerdeteksi si bocah *${name}* telah mengirim bug ℛ !\n\nMaaf Bocah Bug ℛ Tidak Berguna Bagi Bot!`)
+   await this.modifyChat(m.chat, 'clear', {
+            includeStarred: false
+        }).catch(console.log)
+  }
+}
+
+module.exports = handler
