be53f382eaed5975635a46ccb250a3d00390ddbc
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 10:31:33 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -1,5 +1,4 @@
-const fs = require('fs')
-const fetch = require('node-fetch')
+let fetch = require('node-fetch')
 
 let timeout = 120000
 let poin = 500
@@ -10,20 +9,20 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
     throw false
   }
-  let tebakgambar = JSON.parse(fs.readFileSync(`./src/tebakgambar.json`))
-  let json = tebakgambar[Math.floor(Math.random() * tebakgambar.length)]
+  let res = await fetch(global.API('zahir', '/api/kuis/tebakgambar', {}, 'apikey'))
+  if (res.status !== 200) throw await res.text()
+  let json = await res.json()
+  // if (!json.status) throw json
   let caption = `
-  ${json.deskripsi}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hint untuk bantuan
+Ketik ${usedPrefix}hint untuk hint
 Bonus: ${poin} XP
     `.trim()
   conn.tebakgambar[id] = [
-    await conn.sendButtonImg(m.chat, caption, await (await fetch(json.img)).buffer(), 'BOTSTYLE', 'BANTUAN', '.hint')
-    ,
+    await conn.sendFile(m.chat, json.result.images, 'tebakgambar.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
     json, poin,
-    setTimeout(async () => {
-      if (conn.tebakgambar[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'BOTSTYLE', 'TEBAK GAMBAR', '.tebakgambar')
+    setTimeout(() => {
+      if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.tebakgambar[id][0])
       delete conn.tebakgambar[id]
     }, timeout)
   ]
