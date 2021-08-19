6d3ae5d7cbdf880aa4ee431e41b23b49a7b193ee
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 10:33:27 +0700

    Update tebakgambar_hint.js

--- plugins/tebakgambar_hint.js
+++ plugins/tebakgambar_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakgambar)) throw false
     let json = conn.tebakgambar[id][1]
-    conn.reply(m.chat, '```' + json.jawaban.replace(/[AIUEOaiueo]/g, '_') + '```\nBalas gambarnya, bukan pesan ini', conn.tebakgambar[id][0])
+    m.reply('```' + json.result.jawaban.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
 }
 handler.command = /^hint$/i
 
