cc01652d81db22ae718179415f9279a72af61ff3
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 11:33:35 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,7 +1,8 @@
+let limit = 30
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, usedprefix, isPrems, isOwner }) => {
+let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
   if (!text) throw 'Cari apa?'
   let chat = global.DATABASE.data.chats[m.chat]
   let results = await yts(text)
@@ -13,29 +14,41 @@ let handler = async (m, { conn, command, text, usedprefix, isPrems, isOwner }) =
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
+      yt = await (isVideo ? ytv : yta)(vid.url, server)
       usedServer = server
       break
     } catch (e) {
       m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
     }
   }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
+  if (yt === false) throw 'Semua server tidak bisa :/'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonImg(m.chat, `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*Source:* ${vid.url}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 *Server y2mate:* ${usedServer}
-          `.trim(),
-    await (await fetch(thumb)).buffer(), 'BOTSTYLE', 'AUDIO', `${usedprefix}yta ${vid.url}`, 'VIDEO', `${usedprefix}yt ${vid.url}`)
+`.trim(), m)
+let _thumb = {}
+try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+catch (e) { }
+if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*Source:* ${vid.url}
+*Server y2mate:* ${usedServer}
+`.trim(), m, false,  {
+  ..._thumb,
+  asDocument: chat.useDocument
+})
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
-handler.command = /^play$/i
+handler.command = /^play2?$/i
 
 handler.exp = 0
+handler.limit = true
 
 module.exports = handler
