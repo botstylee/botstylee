3ce1657ca52c868e21f98b29682e26dc48e7f5f8
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 10:50:30 +0700

    Update tebaklirik.js

--- plugins/tebaklirik.js
+++ plugins/tebaklirik.js
@@ -1,4 +1,6 @@
-let fs = require('fs')
+const fs = require('fs')
+const fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
 let handler = async (m, { conn, usedPrefix }) => {
@@ -8,19 +10,21 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklirik[id][0])
         throw false
     }
-    let tebaklirik = JSON.parse(fs.readFileSync(`./src/tebaklirik.json`))
-    let json = tebaklirik[Math.floor(Math.random() * tebaklirik.length)]
+    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebaklirik.json')
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
 ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}teli untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.tebaklirik[id] = [
-        await conn.send2Button(m.chat, caption, 'BOTSTYLE', 'BANTUAN', `.teli`, 'NYERAH', 'nyerah'),
+        await conn.reply(m.chat, caption, m),
         json, poin,
-        setTimeout(async () => {
-            if (conn.tebaklirik[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'BOTSTYLE', 'TEBAK LIRIK', `${usedPrefix}tebaklirik`)
+        setTimeout(() => {
+            if (conn.tebaklirik[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebaklirik[id][0])
             delete conn.tebaklirik[id]
         }, timeout)
     ]
