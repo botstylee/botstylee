70633fc72fc7cdbe8fa0053679cc48563673203b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 10:54:05 +0700

    Update tebaklirik_hint.js

--- plugins/tebaklirik_hint.js
+++ plugins/tebaklirik_hint.js
@@ -1,11 +1,9 @@
 let handler = async (m, { conn }) => {
-    conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
+    conn.tebakirik = conn.tebakirik ? conn.tebakirik : {}
     let id = m.chat
-    if (!(id in conn.tebaklirik)) throw false
-    let json = conn.tebaklirik[id][1]
-    let ans = json.jawaban.trim()
-    let clue = ans.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebaklirik[id][0])
+    if (!(id in conn.tebakirik)) throw false
+    let json = conn.tebakirik[id][1]
+    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^teli$/i
 handler.limit = true
