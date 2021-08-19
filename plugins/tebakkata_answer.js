8354ec87e833f98f13465eecd273ef6cfeeac921
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-19 10:42:25 +0700

    Update tebakkata_answer.js

--- plugins/tebakkata_answer.js
+++ plugins/tebakkata_answer.js
@@ -3,26 +3,20 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teka/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teka/i.test(m.quoted.text)) return !0
     this.tebakkata = this.tebakkata ? this.tebakkata : {}
     if (!(id in this.tebakkata)) return m.reply('Soal itu telah berakhir')
-    if (/^nyerah$/i.test(m.text)) {
-        await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.tebakkata[id][1].jawaban))}`.trim(), 'BOTSTYLE', 'TEBAK KATA', '.tebakkata').then(() => {
+    if (m.quoted.id == this.tebakkata[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
+        // m.reply(JSON.stringify(json, null, '\t'))
+        if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
+            global.DATABASE.data.users[m.sender].exp += this.tebakkata[id][2]
+            m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP`)
+            clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
-            throw 0
-        })
+        } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
     }
-    // if (m.quoted.id == this.tebakkata[id][0].id) {
-    let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
-    if (['.teka', 'BANTUAN', ''].includes(m.text)) return !0
-    if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-        global.DATABASE.data.users[m.sender].exp += this.tebakkata[id][2]
-        await this.sendButton(m.chat, `*Benar!* +${this.tebakkata[id][2]} XP`, 'BOTSTYLE', 'TEBAK KATA', '.tebakkata')
-        clearTimeout(this.tebakkata[id][3])
-        delete this.tebakkata[id]
-    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
-    // }
     return !0
 }
 handler.exp = 0
