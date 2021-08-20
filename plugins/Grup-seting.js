121598be1ec9459385fbe9630998f835cd5eb53a
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-20 21:25:04 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 contoh:
 ${usedPrefix + command} tutup
 ${usedPrefix + command} buka
-	`.trim(), '© stikerin', 'BUKA', ',grup 1', 'TUTUP', ',grup 0')
+	`.trim(), 'BOTSTYLE', 'BUKA', ',grup 1', 'TUTUP', ',grup 0')
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
