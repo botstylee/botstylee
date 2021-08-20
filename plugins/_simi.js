a5876ef9aba8d5b55a1064cb8f92427e1afba3e7
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-21 01:51:23 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -9,7 +9,7 @@ handler.before = async (m) => {
         let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(m.text) }, 'apikey'))
         if (!res.ok) throw await `${res.status} ${res.statusText}`
         let json = await res.json()
-        if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
+        if (json.result == 'Aku tidak mengerti apa yang kamu katakan sayang.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
         else await m.reply(`*Simi:* ${json.result}`)
         return !0
     }
