3b9f65b25e3177391d473b1f69ba8f8108b67cbf
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-21 20:04:45 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,9 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
+  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
-  if (json.status) m.reply(json.result)
-  else throw json
+  if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
+  await m.reply(`*Simi:* ${json.result}`)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['game']
