e0b608c2556084d6ec0d0b97d7a319eea95006fe
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-28 19:05:40 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,5 @@
 function handler(m) {
-  // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
-  this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
+  this.sendContact(m.chat, '6282114499086', this.getName('6282114499086@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
