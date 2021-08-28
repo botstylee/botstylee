25cbf2bffb2418ee523dbfbf67f198edc3c1a365
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-28 20:00:48 +0700

    Update stickergif.js

--- plugins/stickergif.js
+++ plugins/stickergif.js
@@ -57,9 +57,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
             .save(ran)
     } else throw gokil
 }
-handler.help = ['stiker2', 'sgif2']
+handler.help = ['stiker2']
 handler.tags = ['sticker']
-handler.command = /^(s(t|k|tic?ker)?2|(s(tic?ker|k|t)?)?gif)?2$/i
+handler.command = /^(s(t|k|tic?ker)?2)$/i
 
 handler.disable = true
 
