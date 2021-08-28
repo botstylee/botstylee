f3faa0dbde048a9eb703ae37dfd1a879d354fbdf
botstylee <85060672+botstylee@users.noreply.github.com>
2021-08-28 20:02:05 +0700

    Update stikerly.js

--- plugins/stikerly.js
+++ plugins/stikerly.js
@@ -6,19 +6,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     if (!text) throw `*Perintah ini untuk mengambil stiker dari Stickerly berdasarkan pencarian*\n\nContoh penggunaan:\n${usedPrefix + command} spongebob`
 
-    let res = await fetch(global.API('xteam', '/sticker/stickerly', { q: text }, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
+    let res = await fetch(global.API('zeks', '/api/searchsticker', { q: text }, 'apikey'))
+    if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
-    m.reply(`
-*Total stiker:* ${json.result.stickerlist.length}
-        `.trim())
+    let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
+    m.reply(`*${json.title}*
+*Estimasi selesai:* ${json.sticker.length * 1.5} detik
+`.trim())
 
-    for (let i of json.result.stickerlist) {
+    for (let i of json.sticker) {
         stiker = await sticker(false, i, global.packname, global.author)
-        await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
+        await conn.sendMessage(m.chat, stiker, MessageType.sticker)
         await delay(1500)
     }
+    m.reply('_*Selesai*_')
 
 }
 handler.help = ['stikerly <pencarian>']
