579cca5820dd8838508d7a0f3008d6ac9662f1d1
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-01 15:27:48 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,20 +1,25 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
-let handler = async (m, { conn, text, isMods, isOwner }) => {
-    let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
-    let [_, code] = link.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
-    if (isMods || isOwner || m.fromMe) {
-        let res = await conn.acceptInvite(code)
-        m.reply(`Berhasil join grup ${res.gid}`)
-    } else {
-        for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply('*dari:* ' + m.sender.split('@')[0] + '\n*Link:* ' + link, jid)
-        m.reply('Sedang di process Owner')
-    }
+let handler = async (m, { conn, text, usedPrefix }) => {
+    let [_, code] = text.match(linkRegex) || []
+    if (!code) throw 'Link Salah'
+    let res = await conn.acceptInvite(code)
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+        var jumlahHari = 86400000 * 0.5
+        var now = new Date() * 1
+        if (now < global.DATABASE.data.chats[res.gid].expired) global.DATABASE.data.chats[res.gid].expired += jumlahHari
+        else global.DATABASE.data.chats[res.gid].expired = now + jumlahHari
+    })
+    await conn.sendButton(res.gid, `
+*${conn.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, *${conn.user.name}* diundang oleh @${m.sender.split`@`[0]}
+    
+ketik *${usedPrefix}menu* untuk melihat daftar perintah`.trim(), 'BOTSTYLE', 'Menu', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
 }
-handler.help = ['join [chat.whatsapp.com]']
-handler.tags = ['premium']
+handler.help = ['join <chat.whatsapp.com>']
+handler.tags = ['']
 
 handler.command = /^join$/i
+
+handler.premium = false
 
 module.exports = handler
