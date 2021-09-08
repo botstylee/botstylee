be552919ae3ff5e62f286479fc922cccf1f21f5a
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-08 10:28:17 +0700

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -9,15 +9,15 @@ handler.before = async function (m) {
     // if (m.quoted.id == this.math[id][0].id) {
     let math = JSON.parse(JSON.stringify(this.math[id][1]))
     if (m.text == math.result) {
-      global.DATABASE.data.users[m.sender].exp += math.bonus
+      global.db.data.users[m.sender].exp += math.bonus
       clearTimeout(this.math[id][3])
       delete this.math[id]
-      await this.sendButton(m.chat, `*Benar!* +${math.bonus} XP`, '', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
+      await this.sendButton(m.chat, `*Benar!* +${math.bonus} XP`, '© stikerin', `${math.mode.toUpperCase()}`, `.math ${math.mode}`, m)
     } else {
       if (--this.math[id][2] == 0) {
         clearTimeout(this.math[id][3])
         delete this.math[id]
-        await this.sendButton(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, '', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
+        await this.sendButton(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, 'BOTSTYLE', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
       } else m.reply(`*Jawaban Salah!*\nMasih ada ${this.math[id][2]} kesempatan`)
     }
   } catch (e) { return }
