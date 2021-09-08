dc6abc068ebcccf7f6d5a5fe2471cc0a6c489a1f
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-08 11:19:25 +0700

    Update tebakkimia.js

--- plugins/tebakkimia.js
+++ plugins/tebakkimia.js
@@ -18,7 +18,7 @@ Ketik ${usedPrefix}teki untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkimia[id] = [
-        await conn.sendButton(m.chat, caption, 'BOTSTYLE', 'Bantuan', '.teki'),
+        await conn.sendButton(m.chat, caption, 'BOTSTYLE', 'Bantuan', '.teki', m),
         json, poin,
         setTimeout(async () => {
             if (conn.tebakkimia[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.unsur}*`, 'BOTSTYLE', 'Tebak Kimia', '.tebakkimia', conn.tebakkimia[id][0])
