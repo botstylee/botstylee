4577315db8fc585bc0dac6b763c4d4846ac2dfe3
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-08 09:22:15 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,14 +1,19 @@
 let handler = m => m
 
 handler.all = async function (m) {
+    if (!DATABASE.data.settings[this.user.jid].antispam) return // antispam aktif?
+    if (m.isBaileys && m.fromMe) return
     if (!m.message) return
+    if (!m.isCommand) return
+    if (DATABASE.data.users[m.sender].banned) return
+    if (DATABASE.data.chats[m.chat].isBanned) return
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
-                //global.DATABASE._data.users[m.sender].banned = true
-                m.reply('*Jangan Spam!!*')
+                DATABASE.data.users[m.sender].banned = true
+                await this.sendButton(m.chat, 'kamu dibanned karena spam!', 'BOTSTYLE', 'OWNER', ',owner', m)
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
