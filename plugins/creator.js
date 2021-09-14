02575c37b3ee0b6164a76e6a2f714749f758cc49
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-14 13:43:29 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,11 +1,7 @@
-let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  let contacts = []
-  for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
-    contacts.push(...[owner[0], owner[1].name])
-  }
-  if (contacts.length < 3) return this.sendContact(m.chat, contacts[0], contacts[1], m)
-  this.sendContactArray(m.chat, contacts, m)
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '6282114499086', 'BENNIISMAEL', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
