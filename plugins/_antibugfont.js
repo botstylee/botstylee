58f4e8d06869a22a79492d3d73b6bfd47a4a5d7c
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-19 11:09:33 +0700

    Update _antibugfont.js

--- plugins/_antibugfont.js
+++ plugins/_antibugfont.js
@@ -7,9 +7,9 @@ handler.before = function (m, { user, isAdmin, isBotAdmin }) {
   if (m.isBaileys && m.fromMe) throw false
   let chat = global.DATABASE.data.chats[m.chat]
   let name = this.getName(m.sender)
-  let isGroupFont = linkRegex.exec(m.text)
+  let isGroup = linkRegex.exec(m.text)
 
-  if (chat.antiBugfont && isGroupFont) {
+  if (chat.antiBugfont && isGroup) {
  //m.reply(`*「 ANTI BUG ℛ 」*\n\nTerdeteksi *${name}* bocah telah mengirim bug ℛ !\n\nMaaf Bocah Bug ℛ Tidak Berguna Bagi Bot!`)
   //this.groupRemove(m.chat, [m.sender])
     await this.modifyChat(m.chat, 'clear', {
