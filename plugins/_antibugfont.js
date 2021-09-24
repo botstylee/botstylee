bda93335d93cca8aca470e780d581762d8840210
botstylee <85060672+botstylee@users.noreply.github.com>
2021-09-25 02:16:15 +0700

    Update _antibugfont.js

--- plugins/_antibugfont.js
+++ plugins/_antibugfont.js
@@ -10,12 +10,8 @@ handler.before = function (m, { user, isAdmin, isBotAdmin }) {
   let isGroupFont = linkRegex.exec(m.text)
 
   if (chat.antiBugfont && isGroupFont) {
- //m.reply(`*「 ANTI BUG ℛ 」*\n\nTerdeteksi *${name}* bocah telah mengirim bug ℛ !\n\nMaaf Bocah Bug ℛ Tidak Berguna Bagi Bot!`)
-  //this.groupRemove(m.chat, [m.sender])
-  //this.reply('6282114499086@s.whatsapp.net', `PELAKU BUG FONT @${m.sender.split`@`[0]}`, null, { contextInfo: { mentionedJid: [m.sender] } })
-    await this.modifyChat(m.chat, 'clear', {
-            includeStarred: false
-        }).catch(console.log)
+ m.reply(`*「 ANTI BUG ℛ 」*\n\nTerdeteksi *${name}* bocah telah mengirim bug ℛ !\n\nMaaf Bocah Bug ℛ Tidak Berguna Bagi Bot!`)
+  this.groupRemove(m.chat, [m.sender])
     }
 }
 
