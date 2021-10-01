64d04411d02924a2cabea12ff07d6db7e8deb6f1
botstylee <85060672+botstylee@users.noreply.github.com>
2021-10-02 01:28:05 +0700

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -18,7 +18,7 @@ Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' '
     `.trim()
     this.game[id] = {
         id,
-        msg: await this.sendButton(m.chat, caption, 'BOTSTYLE', 'Nyerah', 'nyerah', m),
+        msg: await this.sendButton(m.chat, caption, 'BOTSTYLE', 'Nyerah', '.nyerah', m),
         ...json,
         terjawab: Array.from(json.jawaban, () => false),
         winScore,
