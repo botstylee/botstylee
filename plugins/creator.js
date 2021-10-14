d0bce5f4052a97ce38ed54119037377601b4c7d8
botstylee <85060672+botstylee@users.noreply.github.com>
2021-10-14 11:52:55 +0700

    Fix yang error

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ let handler = function (m) {
   for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
     contacts.push(...[owner[0], owner[1].name])
   }
-  if (contacts.length < 3) return this.sendContact(m.chat, contacts[0], contacts[1], m)
+  if (contacts.length < 1) return this.sendContact(m.chat, contacts[0], contacts[1], m)
   this.sendContactArray(m.chat, contacts, m)
 }
 handler.help = ['owner', 'creator']
