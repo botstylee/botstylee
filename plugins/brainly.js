7490918dbfa15474880880b16913e030d0ac7d6e
botstylee <85060672+botstylee@users.noreply.github.com>
2021-10-20 10:40:59 +0700

    Create brainly.js

---
+++ plugins/brainly.js
@@ -0,0 +1,19 @@
+const brainly = require('brainly-scraper-v2')
+let handler = async function (m, { text }) {
+  if (!text) throw 'Soalnya?'
+  let res = await brainly(text)
+  let answer = res.data.map((v, i) => `
+_*PERTANYAAN KE ${i + 1}*_
+${v.pertanyaan}${v.jawaban.map((v,i) => `
+*JAWABAN KE ${i + 1}*
+${v.text}`).join``}`).join(`
+•------------•
+`)
+  m.reply(answer)
+}
+handler.help = ['brainly <soal>']
+handler.tags = ['internet']
+
+handler.command = /^brainly$/i
+
+module.exports = handler
