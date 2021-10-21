9073e254ac895c9cec4fcacb0ab6ac98b948c30f
botstylee <85060672+botstylee@users.noreply.github.com>
2021-10-21 10:20:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
 if (opts['debug']) conn.logger.level = 'debug'
-if (opts['big-qr'] || opts['server']) conn.on('qr', qr => generate(qr, { small: false }))
+if (opts['big-qr']) conn.on('qr', qr => generate(qr, { small: false }))
 let lastJSON = JSON.stringify(global.DATABASE.data)
 if (!opts['test']) setInterval(() => {
   conn.logger.info('Saving database . . .')
