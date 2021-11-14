beb1cafe265bd2174bb8232b4995af1422e5ee3b
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:32:49 +0700

    Update textpro-carbon.js

--- plugins/textpro-carbon.js
+++ plugins/textpro-carbon.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['carbon <text>']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^carbon$/i
 handler.owner = false
 handler.mods = false
