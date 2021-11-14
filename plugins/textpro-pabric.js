7653300af04d4bdcf415f26d0cf5fcb14573583d
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:47:05 +0700

    Update textpro-pabric.js

--- plugins/textpro-pabric.js
+++ plugins/textpro-pabric.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['pabrik [text]']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^pabrik$/i
 handler.owner = false
 handler.mods = false
