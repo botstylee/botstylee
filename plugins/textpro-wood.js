31db32f9f08bf5b9d4a1dbb201d47033aa5072e0
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:50:24 +0700

    Update textpro-wood.js

--- plugins/textpro-wood.js
+++ plugins/textpro-wood.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['kayu [text]']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^kayu$/i
 handler.owner = false
 handler.mods = false
