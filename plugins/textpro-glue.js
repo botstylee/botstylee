8c67041bb3f4b8be7d2379d1004f5de419f65610
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:37:33 +0700

    Update textpro-glue.js

--- plugins/textpro-glue.js
+++ plugins/textpro-glue.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['glue <text>']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^glue$/i
 handler.owner = false
 handler.mods = false
