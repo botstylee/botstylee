672b8e51a6212528e9e212df35ed9008428d36e3
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-14 13:36:19 +0800

    sticker gura ?

---
+++ plugins/sgura.js
@@ -0,0 +1,25 @@
+const { MessageType } = require('@adiwajshing/baileys')
+const { sticker } = require('../lib/sticker')
+let handler = async (m, { conn, text }) => {
+if (text.length>7) throw 'Maksimal 7 huruf'
+if (!text) text = await conn.getName(m.sender)
+await m.reply(global.wait)
+  let apiUrl = global.API( 'http://hardianto-chan.herokuapp.com', '/api/bot/gura', {
+    apikey: 'hardianto',
+    nama: text
+  })
+try {
+    let stiker = await sticker(null, apiUrl, 'Guraa', global.author)
+    await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  } catch (e) {
+    m.reply('Conversion failed')
+    await conn.sendFile(m.chat, apiUrl, 'image.png', null, m)
+  }
+}
+handler.command = /^gura$/i
+handler.register = true
+handler.limit = 2
+// Semoga di acc  >\\< //
+module.exports = handler
