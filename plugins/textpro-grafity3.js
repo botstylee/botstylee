c9c534806d6b186c8a59eee890c44faf798ce962
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:40:29 +0700

    Update textpro-grafity3.js

--- plugins/textpro-grafity3.js
+++ plugins/textpro-grafity3.js
@@ -16,7 +16,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['grafity3 <text>']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^grafity3$/i
 handler.owner = false
 handler.mods = false
