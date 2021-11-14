b2e54d4109ee984539a6fc2b03d82c8757e33124
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:49:50 +0700

    Update textpro-stone.js

--- plugins/textpro-stone.js
+++ plugins/textpro-stone.js
@@ -18,7 +18,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 
 
 handler.help = ['stone [teks]']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^stone$/i
 handler.owner = false
 handler.mods = false
