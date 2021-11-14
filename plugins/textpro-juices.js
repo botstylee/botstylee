1fac7b764b7c4ad7eec19a00921b36d344ce35b9
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:42:23 +0700

    Update textpro-juices.js

--- plugins/textpro-juices.js
+++ plugins/textpro-juices.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['jus <text>']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^jus$/i
 handler.owner = false
 handler.mods = false
