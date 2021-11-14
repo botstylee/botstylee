abf4c655b98d5473205f90f0262dc1fd18d11221
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:27:33 +0700

    Update textpro-bokeh.js

--- plugins/textpro-bokeh.js
+++ plugins/textpro-bokeh.js
@@ -16,7 +16,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['bokeh [Teks]']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^bokeh?$/i
 handler.owner = false
 handler.mods = false
