38f3637f960760f5a08efd71c490397518cd6fc8
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 11:41:12 +0700

    Update textpro-jewerly.js

--- plugins/textpro-jewerly.js
+++ plugins/textpro-jewerly.js
@@ -17,7 +17,7 @@ conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!
 .catch(error => console.log(error));
 }
 handler.help = ['jewerly <text>']
-handler.tags = ['te']
+handler.tags = ['textpro']
 handler.command = /^jewerly$/i
 handler.owner = false
 handler.mods = false
