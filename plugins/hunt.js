ca65f9cb7eb6959cffc673899ff84505ea0ce384
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-20 10:31:36 +0700

    Update hunt.js

--- plugins/hunt.js
+++ plugins/hunt.js
@@ -38,33 +38,37 @@ let handler = async (m, { conn, text }) => {
 		{ area: 12, name: "Not so young Dragon" },
 		{ area: 12, name: "Scaled Kid Dragon" },
 		{ area: 13, name: "Definitely not so young Dragon" },
-		{ area: 13, name: "Teen Dragon" },
+		{ area: 13, name: "Teen Dragon*" },
 		{ area: 13, name: "Scaled Teen Dragon" },
 	]
 	let player = global.DATABASE.data.users[m.sender]
 	let pname = conn.getName(m.sender)
 
-	let cdm = `${MeNit(new Date - player.Thunt)}`
-	let cds = `${DeTik(new Date - player.Thunt)}`
+	let cdm = `${MeNit(new Date - player.lasthunt)}`
+	let cds = `${DeTik(new Date - player.lasthunt)}`
 	let cd1 = Math.ceil(01 - cdm)
 	let cd2 = Math.ceil(60 - cds)
 
-	let area_monsters = monsters.filter(monster => { return monster.area === player.area })
+
+	let areaPlayer = monsters.map(v => v.area)
+    areaPlayer = areaPlayer[Math.floor(Math.random() * areaPlayer.length)]
+	let area_monsters = monsters.filter(monster => { return monster.area === areaPlayer })
 	let monster = area_monsters[Math.floor(Math.random() * area_monsters.length)]
 	let monsterName = monster.name.toUpperCase()
+    
 
-	if (new Date - global.DATABASE._data.users[m.sender].lasthunt > 120000) {
-		let coins = parseInt(Math.floor(Math.random() * 401))
-		let exp = parseInt(Math.floor(Math.random() * 601))
-		let sum = 82 * player.area - 59
+	if (new Date -  global.DATABASE.data.users[m.sender].lasthunt > 120000) {
+		let sum = 10 * areaPlayer - 59
 		let dmg = (player.sword  * 5 + player.armor * 5 - sum)
 		dmg = dmg < 0 ? Math.abs(dmg) : 0
+		let coins = areaPlayer * 50
+		let exp = areaPlayer * 35
 
 		player.healt -= dmg
 		player.lasthunt = new Date * 1 // waktu hunt 2menit
 
 		if (player.healt < 0) {
-			let msg = `*${pname}* Anda Mati Di Bunuh Oleh ${monsterName}`
+			let msg = `*${pname}* Anda Mati Di Bunuh Oleh *${monsterName}*`
 			if (player.level > 0) {
 				player.level -= 1
 				msg += `\nLevel Anda Turun 1 Karena Mati Saat Berburu!`
@@ -77,14 +81,16 @@ let handler = async (m, { conn, text }) => {
 		player.money += coins * 1
 		player.exp += exp * 1
 
-		let pesan = `*${pname}* Menemukan Dan Membunuh *${monsterName}*\nMendapatkan ${new Intl.NumberFormat('en-US').format(coins)} coins & ${new Intl.NumberFormat('en-US').format(exp)} XP\nBerkurang -${dmg}Hp, Tersisa ${player.healt}/${100}`
+		let pesan = `*${pname}* Menemukan Dan Membunuh *${monsterName}*\nMendapatkan ${new Intl.NumberFormat('en-US').format(coins)} 📀coins & ${new Intl.NumberFormat('en-US').format(exp)} 🎗️XP\nBerkurang -${dmg}💉Hp, Tersisa ${player.healt}/${100}`
 		m.reply(pesan)
-	} else throw `Tunggu *00:${cd1}:${cd2}* Untuk Berburu Lagi`
+	} else throw `Tunggu *${cd1}:${cd2}* Untuk Berburu Lagi`
 }
 
 handler.help = ['hunt']
 handler.tags = ['rpg']
 handler.command = /^hunt/i
+
+handler.disabled = false
 
 handler.fail = null
 
@@ -99,3 +105,7 @@ function DeTik(ms) {
 	let s = isNaN(ms) ? '60' : Math.floor(ms / 1000) % 60
 	return [s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+
+function pickRandom(list) {
+	return list[Math.floor(Math.random() * list.length)]
+  }
