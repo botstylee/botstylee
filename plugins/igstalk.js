ff276cbb52bf1e45aecc522b70df406579b8d183
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-27 15:48:17 +0700

    Add files via upload

---
+++ plugins/igstalk.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} Beni_230`
+
+  let res = await fetch(global.API('zekais', '/igs', { username: args[0] }))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (json.status != 200) throw json
+  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/Beni_230`, m, 0, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
+}
+handler.help = ['igstalk <username>']
+handler.tags = ['tools']
+handler.command = /^(igstalk)$/i
+handler.limit = true
+module.exports = handler
