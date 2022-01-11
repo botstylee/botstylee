656478924cb436cfabc0941efd8a16b5f4480ed0
botstylee <85060672+botstylee@users.noreply.github.com>
2022-01-12 01:22:19 +0700

    Update tebakbendera_answer.js

--- plugins/tebakbendera_answer.js
+++ plugins/tebakbendera_answer.js
@@ -1,20 +1,23 @@
+const similarity = require('similarity')
+const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
-  let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Tebak Bendera 」/i.test(m.quoted.text)) return !0
-  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-  if (!(id in conn.tebakbendera)) return m.reply('Soal itu telah berakhir')
-  if (m.quoted.id == conn.tebakbendera[id][0].id) {
-    let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
-    // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
-      global.db.data.users[m.sender].uang += conn.tebakbendera[id][2]
-      m.reply(`*Benar!*\n+Rp${conn.tebakbendera[id][2]}`)
-      clearTimeout(conn.tebakbendera[id][3])
-      delete conn.tebakbendera[id]
-    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
-  }
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teko/i.test(m.quoted.contentText)) return !0
+    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
+    if (!(id in this.tebakbendera)) return m.reply('Soal itu telah berakhir')
+    if (m.quoted.id == this.tebakbendera[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
+        if (['.tbhint', 'Bantuan', ''].includes(m.text)) return !0
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            await this.sendButton(m.chat, `*Benar!* +${this.tebakbendera[id][2]} XP`, 'BOTSTYLE', 'Tebak Bendera', '.tebakbendera', m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Dikit Lagi!`)
+        else m.reply(`Salah!`)
+    }
+    return !0
 }
 handler.exp = 0
 
