effc35944bf0c545b6067ed24cc899e6b1d1bee7
botstylee <85060672+botstylee@users.noreply.github.com>
2022-01-12 01:26:24 +0700

    Update tebakbendera_hint.js

--- plugins/tebakbendera_hint.js
+++ plugins/tebakbendera_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakbendera)) throw false
     let json = conn.tebakbendera[id][1]
-    let nya = json.result.jawaban
+    let nya = json.jawaban
     let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
     m.reply('```' + nyanya + '```')
 }
