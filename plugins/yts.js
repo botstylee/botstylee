a3a6c8340094f329b72ef2ab3c0b861c45193803
botstylee <85060672+botstylee@users.noreply.github.com>
2022-01-23 11:16:35 +0700

    Ngerapihin dikit aja

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,26 +1,27 @@
 let yts = require('yt-search')
-let handler = async (m, { text }) => {
-  if (!text) return m.reply('Cari apa?')
-  let results = await yts(text)
-  let teks = results.all.map(v => {
+
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} belajar nodejs`
+  let  results  =  await yts  ( text )
+  let  teks  =  results . all . map ( v  =  >
     switch (v.type) {
       case 'video': return `
-*${v.title}* (${v.url})
-Duration: ${v.timestamp}
-Uploaded ${v.ago}
-${v.views} views
+* ${ v . title } * ( ${ v . url } )
+Duration: ${ v . timestamp }
+Uploaded ${ v . ago }
+${ v . views } Audience
       `.trim()
       case 'channel': return `
 *${v.name}* (${v.url})
-_${v.subCountLabel} (${v.subCount}) Subscriber_
-${v.videoCount} video
+_ $ { v . subCountLabel } ( $ { v . subCount } ) Subscriber_
+$ { v . videoCount } video
 `.trim()
     }
   }).filter(v => v).join('\n========================\n')
-  m.reply(teks)
+  m . reply ( text )
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['tools']
-handler.command = /^yts(earch)?$/i
+handler.tags = ['internet']
+handler . command  =  / ^ yts ( earch ) ? $ / i
 
 module.exports = handler
