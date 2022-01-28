8a9244eed88822e6747217d60fc1fb69bcd2333f
botstylee <85060672+botstylee@users.noreply.github.com>
2022-01-29 03:53:53 +0700

    Ngerapihin aja

--- plugins/update.js
+++ plugins/update.js
@@ -1,15 +1,15 @@
 let { execSync } = require('child_process')
-let handler = async (m, { conn, text, isROwner }) => {
+let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/Nurutomo/wabot-aq.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
-    m.reply(stdout.toString())
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+    conn.reply(m.chat, stdout.toString(), m)
   }
 }
 handler.help = ['update']
 handler.tags = ['host']
 handler.command = /^update$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
