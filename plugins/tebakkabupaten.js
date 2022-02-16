6e5c0f079df09eb357310752319b6939434a5635
GhostUi-19 <73119361+Ghost19-ui@users.noreply.github.com>
2022-02-16 16:49:25 +0700

    Update tebakkabupaten.js

--- plugins/tebakkabupaten.js
+++ plugins/tebakkabupaten.js
@@ -8,20 +8,18 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkabupaten[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkabupaten.json')
-    if (!res.ok) throw eror
+    let res = await fetch('https://bsbt-api-rest.herokuapp.com/api/game/tebakkabupaten')
     let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}tekb untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkabupaten[id] = [
-        await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), caption, 'BOTSTYLE', 'Bantuan', '.teka', m),
+        await conn.sendButtonImg(m.chat, await (await fetch(data.result.url)).buffer(), caption, 'BOTSTYLE', 'Bantuan', '.teka', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.title}*`, 'BOTSTYLE', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
+            if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${data.result.title}*`, 'BOTSTYLE', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
             delete conn.tebakkabupaten[id]
         }, timeout)
     ]
