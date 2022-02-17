b92feb69ea46ee3c8ae57dfd82f6c618f0dca64c
Botsergio <99638282+Botsergio@users.noreply.github.com>
2022-02-17 14:45:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/KihEAYjKr04LI4uUrbiiip']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62895368900456': { // put your number here
-    name: 'benniismael',
+  '0821-2413-7452': { // put your number here
+    name: 'sergio',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
