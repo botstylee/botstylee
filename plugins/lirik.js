aa2fde03aaf586937ec178c73d84e48754e4c445
GhostUi-19 <73119361+Ghost19-ui@users.noreply.github.com>
2022-02-27 17:29:39 +0700

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,11 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. cari apa sayang?\n\ncontoh:\n${usedPrefix + command} dandelions`
-  let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
+  let res = await fetch("https://bsbt-api-rest.herokuapp.com/api/lirik?judul="+text)
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
-  if (!json.status) throw json
-  m.reply(json.result)
+  //if (!json.status) 
+  throw json
+  //m.reply(json.result)
+/**
+*
+* a = await fetch("https://tessyy-api.herokuapp.com/lirik?q="+text)
+* if (await a.json()) throw "tidak dapat menemukan lirik tersebut"
+* b = await a.text() 
+* m.reply(b)
+*
+*/
 }
 handler.help = ['lirik'].map(v => v + ' <teks>')
 handler.tags = ['internet']
