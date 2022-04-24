437493b289c83cafe03f60df1f01b4ae14a4ceaf
botstylee <85060672+botstylee@users.noreply.github.com>
2022-04-24 19:45:16 +0700

    Delete plugins directory

--- plugins/script.js
+++
@@ -1,9 +0,0 @@
-let handler  = async (m, { conn, args }) => {
-   m.reply(`BOTSTYLEE Ini Menggunakan Script Dari Github : https://github.com/botstylee/botstylee`)
-}
- 
-handler.help = ['script']
-handler.tags = ['about']
-handler.command = /^(script)$/i
-
-module.exports = handler
