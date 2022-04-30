75c83d20d760c5852481fb7345e0c76aca6c623f
botstylee <85060672+botstylee@users.noreply.github.com>
2022-04-30 12:54:36 +0700

    Update canvas_affect.cjs

--- plugins/canvas_affect.cjs
+++ plugins/canvas_affect.cjs
@@ -1,13 +1,11 @@
-const uploadImage = require('../lib/uploadImage')
-const uploadFile = require('../lib/uploadFile')
-let fetch = require('node-fetch')
+const uploadImage = require('../lib/uploadImage.cjs')
+const uploadFile = require('../lib/uploadFile.cjs')
 let handler = async (m, { conn,usedPrefix, command, args ,text}) => {  
 try {
   let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)|webp/.test(mime)) throw `Mime ${mime} tidak support`
-  let img = await q.download()
+  let mime = (q.msg || q).mimetype || q.mediaType || ''
+  if (!/webp|image/g.test(mime)) throw `balas gambar/stiker dengan perintah ${usedPrefix + command}`
+  let img = await q.download?.()
   let url = await uploadImage(img).catch(e => uploadFile(img))
   let p = await (await fetch('https://tessyy-api.herokuapp.com/canvas/affect?url='+encodeURIComponent(url) )).buffer()
   m.reply('_Tunggu Sebentar. . ._')
