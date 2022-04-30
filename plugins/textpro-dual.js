f2aa9d2c26b26a82f053a867933891a44242f11e
botstylee <85060672+botstylee@users.noreply.github.com>
2022-04-30 14:34:04 +0800

    Update textpro-dual.js

--- plugins/textpro-dual.js
+++ plugins/textpro-dual.js
@@ -1,50 +1,34 @@
-const textpro = require("../lib/textpro.js");
-const axios = require ("axios")
-const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, args}) => {
-    let txt = args.join` `
-    	  if (!txt) return conn.reply(m.chat, 'harap masukan teksnya!!!', m)
-    textpro("https://textpro.me/metal-purple-dual-effect-973.html", [`${txt}`])
-.then(async (data) => {
-let au = `${data}`
-console.log(data)
-let buf = await getBuffer(data);
-conn.reply(m.chat, '_tunggu sebentar. . ._',m)
-conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!!!'} )
-})
-.catch(error => console.log(error));
-}
-handler.help = ['dual <text>']
-handler.tags = ['textpro']
-handler.command = /^dual$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.admin = false
-handler.botAdmin = false
-handler.fail = null
-handler.limit = true
-handler.exp = 15
-module.exports = handler
-
-const getBuffer = async (url, options) => {
+var tp = require("../lib/textpro.cjs")
+let handler = async (m, {
+	conn,
+	args
+}) => {
+	let txt = args.join` `
+	if (!txt) return conn.reply(m.chat, 'harap masukan teksnya!!!', m)
+	if (txt.length > 15) return conn.reply(m.chat, 'maksimal 15', m)
+	var a = await tp("https://textpro.me/metal-purple-dual-effect-973.html", txt)
+	console.log(a)
 	try {
-		options ? options : {}
-		const res = await axios({
-			method: "get",
-			url,
+		var buffer = await require("axios").request(a, {
+			method: "GET",
+			responseType: "arraybuffer",
 			headers: {
-				'DNT': 1,
-                    'User-Agent': 'GoogleBot',
-				'Upgrade-Insecure-Request': 1
-			},
-			...options,
-			responseType: 'arraybuffer'
+				"user-agent": "GoogleBot"
+			}
 		})
-		return res.data
+		console.log(buffer.status)
+		m.reply("tunggu sebentar")
+		conn.sendFile(m.chat, buffer.data, "", "nih bruh", false)
 	} catch (e) {
-		console.log(`Error : ${e}`)
+		if (e.response) {
+			console.log(e.response.statusText)
+			throw "emror bruh"
+		}
 	}
 }
+handler.help = ['dual [text]']
+handler.tags = ['textpro']
+handler.command = /^dual$/i
+
+
+module.exports = handler
