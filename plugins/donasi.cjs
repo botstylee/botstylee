9de2c2c5c0c531130e4b440af52766c13c54e628
botstylee <85060672+botstylee@users.noreply.github.com>
2022-06-28 12:24:57 +0700

    Update donasi.cjs

--- plugins/donasi.cjs
+++ plugins/donasi.cjs
@@ -2,6 +2,9 @@ let handler = async m => m.reply(`
 ╭─「 Donasi 」
 │ • TELKOMSEL [08211499086]
 │ • DANA [08211499086]
+│ • OVO [08211499086]
+│ • GOPAY [08211499086]
+│ • SHOPPY PAY [08211499086]
 │ • Saweria [https://saweria.co/botstyle]
 ╰────
 ╭─「 Hubungi 」
