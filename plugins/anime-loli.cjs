f536361f260a061a62c664c78cfe481edb8aca8d
botstylee <85060672+botstylee@users.noreply.github.com>
2022-08-17 02:28:43 +0700

    Update anime-loli.cjs

--- plugins/anime-loli.cjs
+++ plugins/anime-loli.cjs
@@ -1,12 +1,14 @@
-var handler = async (m, {
-	conn
-}) => {
-	var hyzer = 'https://api.zacros.my.id/randomimg/loli'
-	//await conn.sendButton(m.chat, hyzer, 'Nih lomli awas ada FBI', wm, [['Loli','.loli']] ,m)
-	conn.sendFile(m.chat, hyzer, 'Nih lomli awas ada FBI', wm, m)
+var fetch = require('node-fetch')
+var handler = async(m, { conn }) => {
+  var res = await fetch(global.API('botstyle', '/api/loli', {}, 'apikey'))
+
+  if (!res.ok) throw await res.text()
+  var json = await res.json()
+  if (!json.result) throw 'Error!'
+  conn.sendButton(m.chat, 'Ini lolinya kak', author, json.result, [['Loli', `.loli`]], m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']
 handler.command = /^(loli)$/i
-var wm = global.wm
+
 module.exports = handler
